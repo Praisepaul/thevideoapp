2e593d3847a324b1b54e583765c9efee8b4f3c45
Praise Paul <praise20024christ@gmail.com>
2020-09-21 22:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  secure: true,
+  path: '/peerjs',
   host: '/',
   port: '443'
 })
